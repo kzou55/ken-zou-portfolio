6a5644a34a87d5ad32f2ff7af9422aff863e5fd8
Ken Zou <kzou55@gmail.com>
2026-08-24 22:09:13 -0400

    implemented scroll to top when navigating between pages/links

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,10 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import ScrollToTop from "../components/ScrollToTop";
 
 const Layout = () => {
   return (
     <div className="min-h-screen flex flex-col">
+      <ScrollToTop />
+
       <Navbar />
 
       <main className="flex-1 pt-16">
